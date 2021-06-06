d30728eada9b003443b892a889fa2a8e813676fa
karakhanov <islommamatov2@gmail.com>
2021-06-06 19:26:43 +0500

    sfd

--- app/admin.py
+++ app/admin.py
@@ -7,3 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import *
+admin.register(User_info)
+admin.site.register(User_info)
--- app/views.py
+++ app/views.py
@@ -9,10 +9,12 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django import template
+from .models import *
 
 @login_required(login_url="/login/")
 def index(request):
-    return render(request, "index.html")
+    user = User_info.objects.all()
+    return render(request, "index.html", {'u': user})
 
 @login_required(login_url="/login/")
 def pages(request):
